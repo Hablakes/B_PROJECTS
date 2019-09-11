ff5a329256655c240a04a143078c1714fe1687b9
Blair OT <45849760+Hablakes@users.noreply.github.com>
2019-09-11 18:03:43 -0400

    Add files via upload

--- B_PROJECTS/B_TEST_1.py
+++ B_PROJECTS/B_TEST_1.py
@@ -6,7 +6,14 @@ data_test_1 = []
 with open(r'C:/Users/botoole/NEWTEST_MEDIA_INDEX/NEWTEST_USER_INFO.json') as f:
     a = json.load(f)
     print(a)
-
-print('\n', '#' * 100, '\n')
-for list_lines in data_test_0:
-    print(list_lines)
+    print('\n', '#' * 100, '\n')
+    print(a['user:'])
+    print('\n', '#' * 100, '\n')
+    print(a['movie_dir:'])
+    print('\n', '#' * 100, '\n')
+    print(a['tv_dir:'])
+    print('\n', '#' * 100, '\n')
+    print(a['movie_alt_dir:'])
+    print('\n', '#' * 100, '\n')
+    print(a['tv_alt_dir:'])
+    print('\n', '#' * 100, '\n')
