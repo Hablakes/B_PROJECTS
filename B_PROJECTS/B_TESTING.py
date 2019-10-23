641554236765931991fbabeb5ab6dc1bba9ca94c
Blair OT <45849760+Hablakes@users.noreply.github.com>
2019-10-22 22:48:34 -0400

    Add files via upload

--- B_PROJECTS/B_TESTING.py
+++ B_PROJECTS/B_TESTING.py
@@ -8,30 +8,53 @@ def match_similar_strings(a, b):
     return SequenceMatcher(None, a, b).ratio()
 
 
+def separator_1():
+    print('-' * 100)
+
+
+def separator_2():
+    for items in '\n', '-' * 100:
+        print(items)
+
+
+def separator_3():
+    for items in '\n', '-' * 100, '\n':
+        print(items)
+
+
 test_list = []
 
 movie_to_search = r'https://www.imdb.com/search/title/?title=' + str("The matrix")
-
-movie_plot_search = r'https://www.imdb.com/search/title-text/?plot=' + str("The matrix")
-
 movie_response = requests.get(movie_to_search, timeout=5)
-
 movie_imdb_page = BeautifulSoup(movie_response.text, 'html.parser')
 movie_body_text = movie_imdb_page.find('div', class_='lister list detail sub-list')
+movie_title_and_id_sections = movie_body_text.find_all(class_='lister-item mode-advanced')
+movie_genres_plot_and_ratings_sections = movie_body_text.find_all(class_='lister-item-content')
 
-movie_body_text_sections = movie_body_text.find_all('div', class_='lister-item')
+for items in movie_title_and_id_sections:
+    title = items.h3.a.text
 
-for items in movie_body_text_sections:
-    test = items.find('img')
-    title = test['alt']
-    imdb_id = test['data-tconst']
+    movie_search_confidence = round(match_similar_strings(title.lower(), str('The Matrix').lower()), 2)
 
-    movie_confidence_percentage = round(match_similar_strings('The Matrix'.lower(),
-                                                              title.lower()), 2)
-
-    if float(movie_confidence_percentage) >= 0.75:
+    if float(movie_search_confidence) >= 0.75:
         movie_title = title
+        certificate = items.find('span', class_='certificate')
+        genre = items.find('span', class_='genre')
+        imdb_id = items.img['data-tconst']
+        plot_section = items.find_all('p')
+        plot = plot_section[1]
+        rating = items.strong
 
-        test_list.append([movie_title, imdb_id])
-
-print(test_list[0])
+        if movie_title:
+            print('TITLE: ', movie_title)
+        if certificate:
+            print('CERTIFICATE: ', certificate.text)
+        if genre:
+            print('GENRE(S): ', genre.text)
+        if imdb_id:
+            print('IMDB ID#: ', imdb_id)
+        if plot:
+            print('PLOT: ', plot.text)
+        if rating:
+            print('RATING: ', rating.text)
+        separator_1()
