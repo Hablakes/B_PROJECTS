2889cce67bcae4fb977a8cd48c4d8ee73335fce5
Blair OT <45849760+Hablakes@users.noreply.github.com>
2019-12-21 16:28:35 -0500

    Add files via upload

--- B_PROJECTS/B_TESTING_1.py
+++ B_PROJECTS/B_TESTING_1.py
@@ -1,1 +1,7 @@
-pass
+test_strings = ["Phillip K. Dick/'s Electric Dreams", "The Orville"]
+
+
+for items in test_strings:
+    if "/'s" in items:
+        new_string = items.rsplit("/'s ", 1)[1]
+        print(new_string)
