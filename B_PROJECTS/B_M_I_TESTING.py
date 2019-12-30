f6321f0786fdd98c4cf635eb7c9080ea2909d17a
Blair OT <45849760+Hablakes@users.noreply.github.com>
2019-12-30 12:14:18 -0500

    Add files via upload

--- B_PROJECTS/B_M_I_TESTING.py
+++ B_PROJECTS/B_M_I_TESTING.py
@@ -104,6 +104,7 @@ def create_tv_show_information_index():
                     elif track.track_type == 'Video':
                         tv_results_list[tv_file[0]]['RESOLUTION'] = str(track.width) + 'x' + str(track.height)
 
+                search_confidence_percentage, tv_info_set = find_imdb_show(tv_title_key)
                 try:
 
                     tv_info_set = None
@@ -373,6 +374,34 @@ def directory_selection():
     except (TypeError, ValueError) as e:
         print('\n', 'INPUT ERROR: ', e, '\n')
         separator_3()
+
+
+def find_imdb_show(show_name):
+    ia = IMDb()
+    search_confidence_percentage = 0
+
+    tv_imdb = ia.search_movie(show_name)
+
+    possible_tv_show_matches_list = []
+
+    for found_tv_plots in tv_imdb:
+        if found_tv_plots['kind'] != 'tv series':
+            continue
+
+        search_confidence_percentage = match_similar_strings(show_name.lower(),
+                                                             found_tv_plots['title'].lower())
+
+        possible_tv_show_matches = \
+            (found_tv_plots['title'], found_tv_plots.movieID, search_confidence_percentage)
+        possible_tv_show_matches_list.append(possible_tv_show_matches)
+
+    possible_tv_show_matches_list.sort(key=lambda x: x[2], reverse=True)
+
+    if possible_tv_show_matches_list:
+        tv_id = possible_tv_show_matches_list[0][1]
+        tv_info_set = ia.get_movie(tv_id)
+        return search_confidence_percentage, tv_info_set
+    return None
 
 
 def launch_media_index():
