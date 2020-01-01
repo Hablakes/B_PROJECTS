6098c444b4cc7f08feae6c59fd6097700b77c0e1
Blair OT <45849760+Hablakes@users.noreply.github.com>
2020-01-01 12:56:47 -0500

    Add files via upload

--- B_PROJECTS/B_TESTING_1.py
+++ B_PROJECTS/B_TESTING_1.py
@@ -58,31 +58,26 @@ def find_imdb_show(show_name):
         return tv_info_set
 
 
-def tv_shows_index():
-    tv_results_list = {}
+def tv_shows_overview_plots_index():
     tv_overview_plots_dict = {}
-
     tv_folders_list = []
+    tv_overview_plots_nfo_list = []
 
     with open(os.path.expanduser((index_folder + '/TV_VIDEO_FILES_PATHS.csv').format(username)),
               encoding='UTF-8') as m_f_p:
         tv_index = csv.reader(m_f_p)
 
         for tv_file in sorted(tv_index):
+
             tv_filename_key = tv_file[0].rsplit('/', 1)[-1]
-            tv_filetype_key = tv_file[0].rsplit('.', 1)[1]
             tv_title_key = tv_file[0].rsplit('/')[-2].split('(')[0]
-            tv_title_and_year_key = tv_file[0].rsplit('/')[-2]
-            tv_year_key = tv_file[0].rsplit('/')[-2].rsplit('(')[-1][:-1]
 
             if tv_title_key not in tv_folders_list:
+
                 tv_folders_list.append(tv_title_key)
 
-            if tv_file[0] not in tv_results_list:
-                if not tv_filename_key.lower().endswith('.nfo'):
-                    tv_results_list[tv_file[0]] = {}
-
     for found_tv_shows in tv_folders_list:
+
         found_result = find_imdb_show(found_tv_shows)
 
         if found_result:
@@ -91,48 +86,43 @@ def tv_shows_index():
             item_year = found_result.get('year')
 
             if item_title not in tv_overview_plots_dict:
+
                 tv_overview_plots_dict[found_tv_shows] = {}
                 tv_overview_plots_dict[found_tv_shows]['SHOW'] = str(
                     str(item_title) + ' (' + str(item_year) + ')')
 
                 if 'plot' in found_result:
+
                     item_plot = found_result['plot']
                     tv_overview_plots_dict[found_tv_shows]['PLOT'] = item_plot[0].split('::')[0].strip()
 
         if found_tv_shows not in tv_overview_plots_dict:
+
             tv_overview_plots_dict[found_tv_shows] = {}
             tv_overview_plots_dict[found_tv_shows]['SHOW'] = found_tv_shows
             tv_overview_plots_dict[found_tv_shows]['PLOT'] = 'NO PLOT AVAILABLE'
 
-    for items in tv_overview_plots_dict.items():
-        print(items)
+    for found_tv_plots in tv_overview_plots_dict.items():
 
-"""
-            tv_file_size = os.path.getsize(tv_file[0])
-            tv_file_size_in_mb = (int(tv_file_size) / 1048576)
-            tv_file_size_in_mb_rounded = str(round(tv_file_size_in_mb, 2))
-            tv_hash = str(str(tv_filename_key) + '_' + str(tv_file_size))
+        if str(found_tv_plots[1]['SHOW']).lower() == str(tv_title_key).lower():
 
-            tv_results_list[tv_file[0]]['MEDIA-PATH'] = tv_file[0]
-            tv_results_list[tv_file[0]]['MEDIA-TYPE'] = str('TV SHOW')
-            tv_results_list[tv_file[0]]['FOLDER-NAME'] = tv_title_key
-            tv_results_list[tv_file[0]]['FILE-NAME'] = tv_filename_key
-            tv_results_list[tv_file[0]]['FILE-SIZE'] = tv_file_size_in_mb_rounded
-            tv_results_list[tv_file[0]]['TV-HASH'] = tv_hash
-            tv_results_list[tv_file[0]]['FILE-TYPE'] = tv_filetype_key
-            
-    for found_tv_episodes in tv_results_list.items():
-        print(found_tv_episodes)
-        
-    IMDb().update(found_result, 'episodes')
-        
-    for found_tv_show_overviews in found_result:
-        
-"""
+            if str(found_tv_plots[1]['PLOT']).lower() == str('NO PLOT AVAILABLE').lower():
+                tv_overview_plots_nfo_list.append(found_tv_plots)
+
+    for tv_show_plots in tv_overview_plots_dict.items():
+        print(tv_show_plots)
+    separator_3()
+    print(tv_overview_plots_nfo_list)
 
 
 def match_similar_strings(a, b):
     return SequenceMatcher(None, a, b).ratio()
+
+
+def remove_html_tags(text):
+    import re
+    clean = re.compile('<.*?>')
+    return re.sub(clean, '', text)
 
 
 def separator_3():
@@ -140,5 +130,5 @@ def separator_3():
         print(items)
 
 
-tv_shows_index()
+tv_shows_overview_plots_index()
 # print(find_imdb_show('TEST SHOW'))
