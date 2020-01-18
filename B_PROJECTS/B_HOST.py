4e7ff59194acca4942642d4cefd8813fc7d47951
Blair OT <45849760+Hablakes@users.noreply.github.com>
2020-01-18 15:12:03 -0500

    Add files via upload

--- B_PROJECTS/B_HOST.py
+++ B_PROJECTS/B_HOST.py
@@ -36,6 +36,8 @@ def launch_programs():
     system_platform = platform.system()
 
     separator_3()
+    print('B_PROGRAM_TOOL: (Compiling List of Available Programs)')
+    separator_3()
 
     if str(system_platform).lower() in str("Windows").lower():
         for root, dirs, files in os.walk(r'C:/'):
@@ -45,31 +47,37 @@ def launch_programs():
                     available_programs_list.append([program_names, (pathlib.Path(root) / programs).as_posix()])
 
         commands_entered = [get_commands_gui()]
+        print('COMMAND ENTERED: ', commands_entered[0])
 
         for enumeration_number, found_programs in enumerate(available_programs_list):
 
-            if str(commands_entered[0]).lower() == str(found_programs[0]).lower():
-                if len(found_programs[0]) >= 1:
-                    results = found_programs
-                    programs_list.append([enumeration_number, results])
+            if str(commands_entered[0]).lower() in str(found_programs[0]).lower():
+                programs_list.append([enumeration_number, found_programs])
 
-        for found_items in programs_list:
-            print(found_items)
+        if programs_list:
+            print()
+            for found_items in programs_list:
+                print(found_items)
 
-        separator_3()
-        program_selection = input('Select Option # from Listed Programs: ')
-        separator_3()
+            separator_3()
+            program_selection = input('SELECT OPTION # FOR DESIRED PROGRAM: ')
+            separator_3()
 
-        for listed_programs in programs_list:
-            if int(program_selection) == int(listed_programs[0]):
+            for listed_programs in programs_list:
+                if int(program_selection) == int(listed_programs[0]):
 
-                os.startfile(str(listed_programs[1][1]))
-                engine.say('Launching:' + str(listed_programs[1][0]))
-                engine.runAndWait()
-                engine.stop()
+                    os.startfile(str(listed_programs[1][1]))
+                    engine.say('Launching:' + str(listed_programs[1][0]))
+                    engine.runAndWait()
+                    engine.stop()
+
+        elif not programs_list:
+            separator_3()
+            print('NO PROGRAM FOUND: (Restarting)')
+            launch_programs()
 
     else:
-        print("Still working on Linux")
+        print("STILL WORKING ON LINUX")
 
 
 def separator_3():
