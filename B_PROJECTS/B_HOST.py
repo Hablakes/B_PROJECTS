6c93068850f8a4740755365086140d314546fea0
Blair OT <45849760+Hablakes@users.noreply.github.com>
2020-01-18 11:43:56 -0500

    Add files via upload

--- B_PROJECTS/B_HOST.py
+++ B_PROJECTS/B_HOST.py
@@ -1,13 +1,15 @@
+import os
+import pathlib
 import platform
 import pyttsx3
+
 from tkinter import Button, Entry, Label, Tk, mainloop
 
 engine = pyttsx3.init()
 
 
 def main():
-    while True:
-        launch_programs()
+    launch_programs()
 
 
 def get_commands_gui():
@@ -28,17 +30,51 @@ def get_commands_gui():
 
 def launch_programs():
     available_programs_list = []
-    commands_entered = [get_commands_gui()]
+
+    programs_list = []
 
     system_platform = platform.system()
 
+    separator_3()
+
     if str(system_platform).lower() in str("Windows").lower():
-        engine.say(str(commands_entered))
-        engine.runAndWait()
-        engine.stop()
+        for root, dirs, files in os.walk(r'C:/'):
+            for programs in files:
+                if programs.lower().endswith(r'.exe'):
+                    program_names = programs[:-4]
+                    available_programs_list.append([program_names, (pathlib.Path(root) / programs).as_posix()])
+
+        commands_entered = [get_commands_gui()]
+
+        for enumeration_number, found_programs in enumerate(available_programs_list):
+
+            if str(commands_entered[0]).lower() == str(found_programs[0]).lower():
+                if len(found_programs[0]) >= 1:
+                    results = found_programs
+                    programs_list.append([enumeration_number, results])
+
+        for found_items in programs_list:
+            print(found_items)
+
+        separator_3()
+        program_selection = input('Select Option # from Listed Programs: ')
+        separator_3()
+
+        for listed_programs in programs_list:
+            if int(program_selection) == int(listed_programs[0]):
+
+                os.startfile(str(listed_programs[1][1]))
+                engine.say('Launching:' + str(listed_programs[1][0]))
+                engine.runAndWait()
+                engine.stop()
 
     else:
         print("Still working on Linux")
+
+
+def separator_3():
+    for item in '\n', '-' * 100, '\n':
+        print(item)
 
 
 if __name__ == '__main__':
