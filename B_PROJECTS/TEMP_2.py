6b4e0da54c242413c20854fa84961ea4d8b07525
Blair OT <45849760+Hablakes@users.noreply.github.com>
2020-05-31 09:42:12 -0400

    Add files via upload

--- B_PROJECTS/TEMP_2.py
+++ B_PROJECTS/TEMP_2.py
@@ -1,1 +1,20 @@
-pass
+l1 = ['Ex Machina', 'The Matrix', 'Westworld', ]
+l2 = ['Ex Machina', 'Ghost In The Shell', 'The Matrix', 'Westworld', 'ZTF']
+
+
+def compare_completed_results(results_one, results_two):
+    output_one = []
+
+    for line in results_one:
+        if line not in results_two:
+            output_one.append('REMOVAL: ' + line)
+
+    for line in results_two:
+        if line not in results_one:
+            output_one.append('ADDITION: ' + line)
+
+    return output_one
+
+
+print(compare_completed_results(l2, l1))
+
