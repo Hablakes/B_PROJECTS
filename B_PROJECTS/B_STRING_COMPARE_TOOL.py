970fc6a9db71fcb1d13a8d411605b165fbc4f2c1
Blair OT <45849760+Hablakes@users.noreply.github.com>
2024-03-21 10:54:46 -0400

    Add files via upload

---
+++ B_PROJECTS/B_STRING_COMPARE_TOOL.py
@@ -0,0 +1,14 @@
+text_1 = input("TEXT #1: ")
+text_2 = input("TEXT #2: ")
+
+print(str("----------"))
+
+for letters in text_1:
+    print(ord(letters))
+
+print(str("----------"))
+
+for letters in text_2:
+    print(ord(letters))
+
+print(str("----------"))
